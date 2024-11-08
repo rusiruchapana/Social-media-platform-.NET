a086f505a85252ab1cdec845f3a3eb9f7c5ec91c
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-11-08 09:05:23 +0530

    convert hashtag class into entity.

--- SocialMediaPlatform/Models/HashTag.cs
+++ SocialMediaPlatform/Models/HashTag.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SocialMediaPlatform.Models;
 
 public class HashTag
 {
+    [Key]
     public int HashTagId { get; set; }
+    
+    [Required]
+    [MaxLength(30)]
     public string Name { get; set; }
+    
+    
     public ICollection<Post> Posts { get; set; }
 }
