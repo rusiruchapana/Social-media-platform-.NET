85c4fc46337df8fe3a7cdd3ca837ac6ceecb7e6f
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-11-08 09:02:22 +0530

    convert like class into entity.

--- SocialMediaPlatform/Models/Like.cs
+++ SocialMediaPlatform/Models/Like.cs
@@ -1,10 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace SocialMediaPlatform.Models;
 
 public class Like
 {
+    [Key]
     public int LikeId { get; set; }
+    
+    [ForeignKey("Post")]
     public int PostId { get; set; }
+    
+    
     public Post Post { get; set; }
+    
+    [ForeignKey("User")]
     public int UserId { get; set; }
+    
+    
     public User User { get; set; }
 }
