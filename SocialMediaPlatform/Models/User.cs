3b4d44c13301bce0add0b208781b494aa1ca5342
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-11-08 08:47:02 +0530

    convert user class into entity.

--- SocialMediaPlatform/Models/User.cs
+++ SocialMediaPlatform/Models/User.cs
@@ -1,15 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SocialMediaPlatform.Models;
 
 public class User
 {
+    [Key]
     public int UserId { get; set; }
+    
+    [Required]
+    [MaxLength(50)]
     public string Username { get; set; }
+    
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
+    
+    [Required]
     public string passwordHash { get; set; }
+    
+    
     public string ProfilePictureUrl { get; set; }
+    
+    
     public DateTime DateCreated { get; set; }
+    
+    
     public ICollection<Follow> Followers { get; set; }
+    
+    
     public ICollection<Follow> Following { get; set; }
+    
+    
     public ICollection<Post> Posts { get; set; }
     
 }
