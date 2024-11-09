2fc57f618f61fbd63081c30b111ee8e3fb5f452b
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-11-09 09:54:25 +0530

    create dto s to register and login.

---
+++ SocialMediaPlatform/DTO/Request/LoginRequestDto.cs
@@ -0,0 +1,7 @@
+namespace SocialMediaPlatform.DTO.Request;
+
+public class LoginRequestDto
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
---
+++ SocialMediaPlatform/DTO/Request/RegisterRequestDto.cs
@@ -0,0 +1,8 @@
+namespace SocialMediaPlatform.DTO.Request;
+
+public class RegisterRequestDto
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
