62a83d1ffa8b3f555e4a181209a180671313906b
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-11-09 08:37:22 +0530

    Update Program.cs

--- SocialMediaPlatform/Program.cs
+++ SocialMediaPlatform/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -22,4 +23,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 app.Run();
