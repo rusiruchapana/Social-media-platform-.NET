4196eeccbac423cc727b2830847af19957b3149e
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-11-28 22:06:25 +0530

    Delete RegisterResponseDTO.cs

--- BlogApp/DTOs/Response/RegisterResponseDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace BlogApp.DTOs.Response;
-
-public class RegisterResponseDTO
-{
-    public int Id { get; set; }
-    public string Username { get; set; }
-    public string Email { get; set; }
-    public DateTime createdAt { get; set; }
-}
