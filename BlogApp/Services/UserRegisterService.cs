1e7e0cd67cde1ad27edf1a4fdac9dc78c514612f
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-11-28 22:15:22 +0530

    Update UserRegisterService.cs

--- BlogApp/Services/UserRegisterService.cs
+++ BlogApp/Services/UserRegisterService.cs
@@ -1,8 +1,14 @@
+using BlogApp.Repositories.Interfaces;
 using BlogApp.Services.Interfaces;
 
 namespace BlogApp.Services;
 
 public class UserRegisterService: IUserRegisterService
 {
-    
+    private readonly IUserRegisterRepository _userRegisterRepository;
+
+    public UserRegisterService(IUserRegisterRepository userRegisterRepository)
+    {
+        _userRegisterRepository = userRegisterRepository;
+    }
 }
