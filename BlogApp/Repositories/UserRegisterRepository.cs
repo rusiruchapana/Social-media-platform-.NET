e798a75e580c8c62886d34e7fc72261e164edacc
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-11-28 22:16:15 +0530

    Update UserRegisterRepository.cs

--- BlogApp/Repositories/UserRegisterRepository.cs
+++ BlogApp/Repositories/UserRegisterRepository.cs
@@ -1,8 +1,14 @@
+using BlogApp.Data;
 using BlogApp.Repositories.Interfaces;
 
 namespace BlogApp.Repositories;
 
 public class UserRegisterRepository: IUserRegisterRepository
 {
-    
+    private readonly ApplicationDbContext _dbContext;
+
+    public UserRegisterRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
 }
