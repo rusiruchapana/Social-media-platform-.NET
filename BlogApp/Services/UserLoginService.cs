04f1ee97056b8d7047c432498232074bfad5f9f9
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-11-29 13:13:31 +0530

    Update UserLoginService.cs

--- BlogApp/Services/UserLoginService.cs
+++ BlogApp/Services/UserLoginService.cs
@@ -1,8 +1,14 @@
+using BlogApp.Repositories.Interfaces;
 using BlogApp.Services.Interfaces;
 
 namespace BlogApp.Services;
 
 public class UserLoginService: IUserLoginService
 {
-    
+    private readonly IUserLoginRepository _userLoginRepository;
+
+    public UserLoginService(IUserLoginRepository userLoginRepository)
+    {
+        _userLoginRepository = userLoginRepository;
+    }
 }
