4760896b631775fc623a3d9f0640b1c8b5576b20
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-11-29 12:44:00 +0530

    Update IUserRegisterService.cs

--- BlogApp/Services/Interfaces/IUserRegisterService.cs
+++ BlogApp/Services/Interfaces/IUserRegisterService.cs
@@ -1,6 +1,9 @@
+using BlogApp.DTOs.Request;
+using BlogApp.DTOs.Response;
+
 namespace BlogApp.Services.Interfaces;
 
 public interface IUserRegisterService
 {
-    
+    Task<UserRegisterResponseDTO> RegisterUser(UserRegisterRequestDTO userRegisterRequestDto);
 }
