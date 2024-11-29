46d5474332252d92c63988ed09edd53c6bfb6eb3
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-11-29 13:07:45 +0530

    Update UserLoginResponseDTO.cs

--- BlogApp/DTOs/Response/UserLoginResponseDTO.cs
+++ BlogApp/DTOs/Response/UserLoginResponseDTO.cs
@@ -2,5 +2,7 @@ namespace BlogApp.DTOs.Response;
 
 public class UserLoginResponseDTO
 {
-    
+    public string Token { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
 }
