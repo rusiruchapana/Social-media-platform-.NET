b365ad8bacb4c36d9510fc51fdb3fca105150425
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-11-29 12:44:11 +0530

    Update IUserRegisterRepository.cs

--- BlogApp/Repositories/Interfaces/IUserRegisterRepository.cs
+++ BlogApp/Repositories/Interfaces/IUserRegisterRepository.cs
@@ -1,6 +1,10 @@
+using BlogApp.Models;
+
 namespace BlogApp.Repositories.Interfaces;
 
 public interface IUserRegisterRepository
 {
-    
+    Task<User> RegisterUser(User user);
+    Task<object> checkUsername(string username);
+    Task<object> checkEmail(string email);
 }
