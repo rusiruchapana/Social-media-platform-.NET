e51530832a528926c592065531c462df2c1bd21e
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-11-29 13:14:32 +0530

    Update UserLoginRepository.cs

--- BlogApp/Repositories/UserLoginRepository.cs
+++ BlogApp/Repositories/UserLoginRepository.cs
@@ -1,8 +1,14 @@
+using BlogApp.Data;
 using BlogApp.Repositories.Interfaces;
 
 namespace BlogApp.Repositories;
 
 public class UserLoginRepository: IUserLoginRepository
 {
-    
+    private readonly ApplicationDbContext _dbContext;
+
+    public UserLoginRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
 }
