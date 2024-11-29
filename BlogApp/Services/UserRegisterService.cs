e28ab49317958d677f73ee89ed8eabe7ab2b6c22
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-11-29 12:44:05 +0530

    Update UserRegisterService.cs

--- BlogApp/Services/UserRegisterService.cs
+++ BlogApp/Services/UserRegisterService.cs
@@ -1,14 +1,41 @@
+using AutoMapper;
+using BlogApp.DTOs.Request;
+using BlogApp.DTOs.Response;
+using BlogApp.Models;
 using BlogApp.Repositories.Interfaces;
 using BlogApp.Services.Interfaces;
+using Microsoft.Win32.SafeHandles;
 
 namespace BlogApp.Services;
 
 public class UserRegisterService: IUserRegisterService
 {
     private readonly IUserRegisterRepository _userRegisterRepository;
+    private readonly IMapper _mapper;
 
-    public UserRegisterService(IUserRegisterRepository userRegisterRepository)
+    public UserRegisterService(IUserRegisterRepository userRegisterRepository , IMapper mapper)
     {
         _userRegisterRepository = userRegisterRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<UserRegisterResponseDTO> RegisterUser(UserRegisterRequestDTO userRegisterRequestDto)
+    {
+        var existingUsername = await _userRegisterRepository.checkUsername(userRegisterRequestDto.Username);
+        if (existingUsername != null)
+            throw new Exception("Username already exists");
+
+        var existingEmail = await _userRegisterRepository.checkEmail(userRegisterRequestDto.Email);
+        if(existingEmail != null)
+            throw new Exception("Email already exists");
+        
+        userRegisterRequestDto.PasswordHash = BCrypt.Net.BCrypt.HashPassword(userRegisterRequestDto.PasswordHash);
+        
+        
+        User user = _mapper.Map<User>(userRegisterRequestDto);
+        User registeredUser = await _userRegisterRepository.RegisterUser(user);
+
+        UserRegisterResponseDTO userRegisterResponseDto = _mapper.Map<UserRegisterResponseDTO>(registeredUser);
+        return userRegisterResponseDto;
     }
 }
