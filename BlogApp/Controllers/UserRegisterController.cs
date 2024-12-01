bf0ab787a6bf0065d68c98bc2f315bc179064575
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-12-01 20:25:00 +0530

    Update UserRegisterController.cs

--- BlogApp/Controllers/UserRegisterController.cs
+++ BlogApp/Controllers/UserRegisterController.cs
@@ -1,12 +1,14 @@
 using BlogApp.DTOs.Request;
 using BlogApp.DTOs.Response;
 using BlogApp.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogApp.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
+[AllowAnonymous]
 public class UserRegisterController: ControllerBase
 {
     private readonly IUserRegisterService _userRegisterService;
