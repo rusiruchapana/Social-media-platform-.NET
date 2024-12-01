4723c0bfe9bb1d6456acdd07250b940d7e8c07ba
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-12-01 20:25:21 +0530

    Update IUserLoginService.cs

--- BlogApp/Services/Interfaces/IUserLoginService.cs
+++ BlogApp/Services/Interfaces/IUserLoginService.cs
@@ -1,6 +1,9 @@
+using BlogApp.DTOs.Request;
+using BlogApp.DTOs.Response;
+
 namespace BlogApp.Services.Interfaces;
 
 public interface IUserLoginService
 {
-    
+    Task<UserLoginResponseDTO> Login(UserRegisterRequestDTO userRegisterRequestDto);
 }
