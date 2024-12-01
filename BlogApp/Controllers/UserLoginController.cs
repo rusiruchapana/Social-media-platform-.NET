34fdf0ff7dfc94bddfc9b43034a2d35155aee9de
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-12-01 20:24:53 +0530

    Update UserLoginController.cs

--- BlogApp/Controllers/UserLoginController.cs
+++ BlogApp/Controllers/UserLoginController.cs
@@ -1,10 +1,14 @@
+using BlogApp.DTOs.Request;
+using BlogApp.DTOs.Response;
 using BlogApp.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogApp.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
+[AllowAnonymous]
 public class UserLoginController: ControllerBase
 {
     private readonly IUserLoginService _userLoginService;
@@ -13,5 +17,20 @@ public class UserLoginController: ControllerBase
     {
         _userLoginService = userLoginService;
     }
-    
+
+    [HttpPost]
+    public async Task<IActionResult> Login(UserRegisterRequestDTO userRegisterRequestDto)
+    {
+        try
+        {
+            UserLoginResponseDTO userLoginResponseDto = await _userLoginService.Login(userRegisterRequestDto);
+            return Ok(userLoginResponseDto);
+        }
+        catch (Exception e)
+        {
+            return Unauthorized(new { message = e.Message });
+        }
+    }
+
+
 }
