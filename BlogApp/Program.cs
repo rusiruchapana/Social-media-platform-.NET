ec64eba8c58e40c9f4f0f3b25cd25409e1be5aa8
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-12-01 20:27:37 +0530

    finish the functionality to login using jwt bearer token and protected api's.

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -12,7 +12,6 @@ using Microsoft.IdentityModel.Tokens;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
