f7a8933a4b05518dfd60f5b3501b4b45fa518113
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-12-02 20:42:20 +0530

    #

--- BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
+++ BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f2ddfdca8a85a7d8773d8d6cdbae28a76277c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54cdb3ea2c5e56f603b08470f467970630143742")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
