88a0048b872cd3504b9dd1b30ace2c11059e8bbb
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-12-02 20:42:29 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import PostPage from './pages/PostPage';
 import RegisterForm from './components/RegisterForm';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path='/register' element={<RegisterForm />} />
-
+            <Route path='/login' element={<LoginForm />} />
             <Route path="/posts/:id" element={<PostPage />} />
         </Routes>
     </Router>
