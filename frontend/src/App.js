841896d98224b70c73d88c46559531a924a6cecd
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-12-02 11:15:56 +0530

    test register page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import PostPage from './pages/PostPage';
+import RegisterForm from './components/RegisterForm';
 
 function App() {
   return (
     <Router>
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path='/register' element={<RegisterForm />} />
+
             <Route path="/posts/:id" element={<PostPage />} />
         </Routes>
     </Router>
---
+++ frontend/src/components/RegisterForm.js
@@ -0,0 +1,9 @@
+function RegisterForm(){
+    return(
+        <div>
+            <h2>Register</h2>
+        </div>
+    );
+}
+
+export default RegisterForm;
