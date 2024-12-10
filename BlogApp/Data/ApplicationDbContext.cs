5edff4d0a2a18f14b2c2fa59262ee06737139a1c
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-12-10 12:49:56 +0530

    Update ApplicationDbContext.cs

--- BlogApp/Data/ApplicationDbContext.cs
+++ BlogApp/Data/ApplicationDbContext.cs
@@ -11,6 +11,6 @@ public class ApplicationDbContext: DbContext
     
     public DbSet<Post> Posts { get; set; }
     public DbSet<Comment> Comments { get; set; }
-    
     public DbSet<User> Users { get; set; }
+    public DbSet<RevokedToken> RevokedTokens { get; set; }
 }
