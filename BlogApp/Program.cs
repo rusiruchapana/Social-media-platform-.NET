25889de8554b2e071ae94530fb40012422e73183
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-12-10 12:49:58 +0530

    Update Program.cs

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -61,6 +61,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidAudience = builder.Configuration["Jwt:Audience"],
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
         };
+        
     });
 
 
